8a20514a65e7f5f6ac2aa024e48dbecbfb300022
midhun <midhunsubramania@gmail.com>
2024-01-25 19:48:32 +0530

    first commit

---
+++ script2.js
@@ -0,0 +1,14 @@
+var a=10;
+var b='10';
+var c=true;
+let arr= [a,b,c];
+console.log(arr);
+// let i=0;
+try{
+while(i<3){
+    console.log(arr[i]);
+    i++;
+}}
+catch(e){
+    console.log('erroris ',e);
+}
