5c857db8742b6302c6eb5c9858986258a06cf440
midhun <midhunsubramania@gmail.com>
2024-02-01 20:44:56 +0530

    script

---
+++ data.js
@@ -0,0 +1,17 @@
+var a=20;
+console.log(a);
+var a="midhun";
+// console.log(a);
+var a=true;
+// console.log(a);
+var a=40.5;
+// console.log(a);
+
+let b=20;
+b="midhun";
+b=true;
+b=40.5;
+console.log(b);
+
+const c="midhun";
+c=40;
